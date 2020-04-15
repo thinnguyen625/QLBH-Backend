7aec6dc78a82ae053c297badb35fd1a58340588e
EricNguyen99 <vanthin7111999@gmail.com>
2020-04-15 23:40:32 +0700

    connected mysql, show data in postman

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # QLBH-Backend
+
+- Mô hình MVC: 
+    - Models: 
+        - Chứa hết tất cả cả hàm xử lý liên quan đến đọc ghi db. 
+        - models sẽ gọi file db và sử dụng các hàm đã được xây dựng, khai báo những câu truy vấn cần thiết
+    - Routes: 
+        - Tương tự như controller, chủ yếu khai báo các đường dẫn, tài nguyên
+        - async - await => muốn gọi được lệnh await thì hàm đó phải trả ra 1 promise
--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,25 @@
 const mysql = require('mysql');
-const {promisify} = require('util');
+const { promisify } = require('util');
 
 const pool = mysql.createPool({
-    connectionLimit: 100,
-    port : 8889,
-    user: 'root',
-    password: 'root',
-    database: 'qlbh'
+   connectionLimit: 100,
+   host: 'localhost',
+   port: 3306,
+   user: 'root',
+   password: 'root',
+   database: 'qlbh'
 });
+
+// module.exports = {
+//    load: sql => {
+//       pool.query(sql, function (error, results, fields) {
+//          if (error)
+//             throw error;
+
+//          console.log('The solution is: ', results[0].solution);
+//       })
+//    }
+// }
 
 const pool_query = promisify(pool.query).bind(pool); // là hàm dạng callback: khi nào nó chạy xong thì nó sẽ đá kích hoạt hàm bên trong để xly ket quả
 
